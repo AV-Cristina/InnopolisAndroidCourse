eb4bea11f4e5d85f15e95e5df62ad87c548b2d4e
AV-Cristina <Cristina.afanasyeva@gmail.com>
2017-06-09 00:56:31 +0300

    Create Main.java

--- Students (Serialization)/src/Models/Main.java
+++ Students (Serialization)/src/Models/Main.java
@@ -10,29 +10,27 @@ import java.util.ArrayList;
  * Created by admin on 08.06.2017.
  */
 public class Main {
-    ArrayList<Students> studentList= new ArrayList<>();
-    
-    Group group1 = new Group(1l, "Android_01");
+    public static void main(String[] args) {
+        Group group1 = new Group(1l, "Android_01");
 
-    ArrayList<Students> studentList = new ArrayList<>();
+        ArrayList<Students> studentList = new ArrayList<>();
 
-    ArrayList<Contact> contacts = new ArrayList<>();
-    
+        ArrayList<Contact> contacts = new ArrayList<>();
+        
 
-    try {
-         FileOutputStream fs = new FileOutputStream("Students.ser");
-         ObjectOutputStream os = new ObjectOutputStream(fs);
-
-        System.out.println("Serializing...");
-
-        for (Students student : studentList)
-        {
-            os.writeObject(student);
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        Date dateOfBirth = null;
+        
+        try {
+            dateOfBirth = format.parse("07.08.1989");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        os.close();
-       }
-       catch (IOException ex){
-        ex.printStackTrace();
-        }
+
+        // String surname, String secondName, Date dateOfBirth, Long groupId, List<Contact> contacts)
+        Students student1 = new Students("Афанасьева Кристина Викторовна", "Кристина", "Афанасьева", "Викторовна", dateOfBirth, group1.getGroupId(), contacts);
+
+        studentList.add(student1);
+    }
 
 }
