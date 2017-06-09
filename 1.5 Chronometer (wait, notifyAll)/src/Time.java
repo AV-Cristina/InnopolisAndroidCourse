70af49f9b143d90d867ecb6a3f319cc676d39fdf
AV-Cristina <Cristina.afanasyeva@gmail.com>
2017-06-10 01:54:46 +0300

    Create Time.java

--- 1.5 Chronometer (wait, notifyAll)/src/Time.java
+++ 1.5 Chronometer (wait, notifyAll)/src/Time.java
@@ -2,11 +2,12 @@
  * Created by Cristina on 09.06.2017.
  */
 public class Time {
-
     private long tm;
+    private int timeout;
 
-    public Time(long tm) {
+    public Time(long tm, int timeout) {
         this.tm = tm;
+        this.timeout = timeout;
     }
 
     public long getTm() {
@@ -15,5 +16,13 @@ public class Time {
 
     public void setTm(long tm) {
         this.tm = tm;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
     }
 }
