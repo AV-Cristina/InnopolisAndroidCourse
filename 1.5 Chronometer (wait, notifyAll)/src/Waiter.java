6d269adf99a9298a2242969c4622f0d4553449ce
AV-Cristina <Cristina.afanasyeva@gmail.com>
2017-06-10 01:56:11 +0300

    Create Waiter.java

--- 1.5 Chronometer (wait, notifyAll)/src/Waiter.java
+++ 1.5 Chronometer (wait, notifyAll)/src/Waiter.java
@@ -1,29 +1,29 @@
-/**
  * Created by Cristina on 09.06.2017.
  */
 public class Waiter implements Runnable{
-    private int interval;
+    private int timeout;
     private Time tm;
 
-    public Waiter(Time tm) {
+    public Waiter(Time tm, int timeout) {
         this.tm = tm;
+        this.timeout = timeout;
     }
 
     @Override
     public void run(){
         synchronized (tm) {
-            for (int i = 0; i < 16; i++) {
+            while (true) {
                 try {
                     //System.out.println("Ждем вызов метода notify у Хронометра");
                     tm.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println("Хронометр передал: " + tm.getTm());
 
                 long cur_tm = tm.getTm();
-                if ((cur_tm % 5000) < 999) {
-                    System.out.println("Прошло 5 секунд");
+
+                if ((cur_tm % (timeout * 1000)) < 999) {
+                    System.out.println("Прошло " + timeout + " секунд");
                 }
             }
         }
