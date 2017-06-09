ba33c5f05b397ea58ddf391a9df3f694c417b07f
AV-Cristina <Cristina.afanasyeva@gmail.com>
2017-06-10 01:55:35 +0300

    Create Chronometer.java

--- 1.5 Chronometer (wait, notifyAll)/src/Chronometer.java
+++ 1.5 Chronometer (wait, notifyAll)/src/Chronometer.java
@@ -2,7 +2,6 @@
  * Created by Cristina on 09.06.2017.
  */
 public class Chronometer implements Runnable {
-
     private Time tm;
 
     public Chronometer(Time tm){
@@ -13,12 +12,13 @@ public class Chronometer implements Runnable {
     public void run() {
         System.out.println("Хронометр запустился");
         long startSession = System.currentTimeMillis();
-        for (int i = 0; i < 16; i++) {
+
+        for (int i = 1; i <= 21; i++) {
             try {
                 Thread.sleep(1000);
                 synchronized (tm) {
                     long timeAfterStart = System.currentTimeMillis() - startSession;
-                    System.out.println(i+1 + ") С начала сессии прошло: " + timeAfterStart);
+                    System.out.println(i + ") С начала сессии прошло: " + timeAfterStart);
                     tm.setTm(timeAfterStart);
                     tm.notifyAll();
                 }
@@ -27,5 +27,4 @@ public class Chronometer implements Runnable {
             }
         }
     }
-
 }
