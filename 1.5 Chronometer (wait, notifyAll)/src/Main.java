5689a450f17c75cd14345a61bc0ae4d75bb900ff
AV-Cristina <Cristina.afanasyeva@gmail.com>
2017-06-10 01:56:43 +0300

    Update Main.java

--- 1.5 Chronometer (wait, notifyAll)/src/Main.java
+++ 1.5 Chronometer (wait, notifyAll)/src/Main.java
@@ -4,10 +4,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        Time tm = new Time(0);
+        Time tm = new Time(0, 0);
 
-        Waiter waiter = new Waiter(tm);
-        new Thread(waiter).start();
+        Waiter waiter5s = new Waiter(tm, 5);
+        new Thread(waiter5s).start();
+
+        Waiter waiter7s = new Waiter(tm, 7);
+        new Thread(waiter7s).start();
 
         Chronometer chronometer = new Chronometer(tm);
         new Thread(chronometer).start();
