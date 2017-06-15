ee86e305ec17510b065a4a820ff3204d5da44208
AV-Cristina <Cristina.afanasyeva@gmail.com>
2017-06-15 23:26:41 +0300

    Create Server.java

--- 1.7 Chat (Sockets)/src/Server.java
+++ 1.7 Chat (Sockets)/src/Server.java
@@ -4,6 +4,9 @@ import java.net.Socket;
 
 /**
  * Created by Cristina on 15.06.2017.
+ * Первым необходимо запускать Сервер
+ * Первым отправку сообщений должен начать Клиент
+ * Для выхода ввести "exit"
  */
 public class Server {
     public static void main(String[] args) throws Exception {
@@ -11,7 +14,7 @@ public class Server {
             ServerSocket serverSocket = new ServerSocket(7777); // создаем сокет сервера
             Socket socket = serverSocket.accept(); // заставляем сервер ждать подключений
 
-            // Берем входной и выходной потоки сокета, теперь можем получать и отсылать данные клиенту
+            // Входной и выходной потоки сокета
             InputStream sin = socket.getInputStream();
             OutputStream sout = socket.getOutputStream();
 
@@ -19,35 +22,25 @@ public class Server {
             DataInputStream in = new DataInputStream(sin);
             DataOutputStream out = new DataOutputStream(sout);
 
-            // Создаем поток для чтения с клавиатуры
             BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
             String line = null;
 
-            boolean isConnect = true;
-
-            while (isConnect) {
+            while (true) {
                 line = in.readUTF(); // ожидаем пока клиент пришлет строку текста и выводим
                 System.out.println("Client : " + line);
 
                 if (line.equals("exit"))
                 {
-                    isConnect = false;
                     serverSocket.close();
                     break;
                 }
                 else {
                     System.out.print("Server : ");
                     line = keyboard.readLine(); // ждем пока пользователь Сервера введет сообщения
-                    if (line.equals("exit")) {
-                        isConnect = false;
-                        serverSocket.close();
-                        break;
-                    }
-                    else {
-                         out.writeUTF(line); // отсылаем клиенту введенную строку
-                         out.flush(); // заставляем поток закончить передачу данных
-                    }
+
+                    out.writeUTF(line); // отсылаем клиенту
+                    out.flush(); // заставляем поток закончить передачу данных
                 }
             }
         } catch (Exception x) {
