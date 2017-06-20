19b75aaa7d19abb0a5aba62fd25e3f7794976568
AV-Cristina <Cristina.afanasyeva@gmail.com>
2017-06-20 22:09:41 +0300

    Update Main.java

--- 1.8 MyWatchService (NIO)/src/Main.java
+++ 1.8 MyWatchService (NIO)/src/Main.java
@@ -4,40 +4,42 @@ import java.util.List;
 /**
  * Created by Cristina on 16.06.2017.
  */
-public class Main {
-    public static void main(String[] args) throws Exception {
-
+public class Main
+{
+    public static void main(String[] args)  throws Exception {
         String filePath = "C:\\Users\\admin\\IdeaProjects\\MyWatchService\\src\\";
         String fileName = "text.txt";
+
+        WatchService watchService = FileSystems.getDefault().newWatchService();
 
         // отслеживаемая дирректория
         Path path = Paths.get(filePath);
 
         printFileContent(filePath, fileName);
 
-        WatchService watchService = FileSystems.getDefault().newWatchService();
-        WatchKey watchKey = path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
-        watchKey = watchService.poll();
+        // отслеживаемые события
+        path.register(
+                watchService,
+                StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_DELETE,
+                StandardWatchEventKinds.ENTRY_MODIFY);
 
+        // если произошло какое-либо из отслеживаемых событий
+        // производится вывод содержимого файла
         WatchKey key;
-
         while ((key = watchService.take()) != null) {
             for (WatchEvent<?> event : key.pollEvents()) {
-
-                System.out.println(
-                        "Event kind:" + event.kind()
-                                + ". File affected: " + event.context() + ".");
-                 printFileContent(filePath, fileName);
-//                if (fileName.equals(event.context())){
-//                    System.out.println("Файл " + fileName + " был изменен:");
-//                    printFileContent(filePath, fileName);
-//                }
+                if (event.context().toString().contains(fileName)){
+                    System.out.println("Файл " + fileName + " был изменен:");
+                    printFileContent(filePath, fileName);
+                }
             }
             key.reset();
         }
     }
 
 
+    // вывод содержимого файла
     public static void printFileContent(String filePath, String fileName){
         String myFullPath = filePath + fileName;
         Path path = Paths.get(myFullPath);
@@ -47,4 +49,5 @@ public class Main {
             System.out.println(content);
         } catch (Exception e) {}
     }
+
 }
