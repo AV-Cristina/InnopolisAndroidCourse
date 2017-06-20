3e96ca4e537fba2ba4f9eb11051faf307e6d072e
AV-Cristina <Cristina.afanasyeva@gmail.com>
2017-06-21 00:47:22 +0300

    Update Kvadrator.java

--- 1.5 Semaphore (Threads)/src/Kvadrator.java
+++ 1.5 Semaphore (Threads)/src/Kvadrator.java
@@ -5,12 +5,12 @@ public class Kvadrator implements Runnable {
     private long y;
     private Consumer consumer;
 
-    public Kvadrator(Consumer consumer, long x) {
+    public Kvadrator(Consumer consumer, long y) {
         this.y = x;
         this.consumer = consumer;
     }
 
     public void run(){
-        consumer.save(y, 0, 0);
+        consumer.save(0, y, 0);
     }
 }
